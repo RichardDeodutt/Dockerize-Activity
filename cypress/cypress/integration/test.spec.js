d9c942b574f8865b74f3de6367849babb95ea1bb
Richard Deodutt <richarddeodutt@gmail.com>
2022-10-30 01:36:13 -0400

    Fix path and special command

--- cypress/cypress/integration/test.spec.js
+++ cypress/cypress/integration/test.spec.js
@@ -1,6 +1,6 @@
 describe('Heading', () => {
     it('has the right title', () => {
-        cy.visit('http://nginx:80;')
+        cy.visit('http://nginx:80')
 
         cy.get('title')
             .invoke('text')
